73414755af819dacce5de34d93eaab5abce88830
kaisic1224 <kaisic1224@gmail.com>
2023-04-22 17:13:29 -0400

    slash commands setup

--- dist/app.js
+++ dist/app.js
@@ -22,9 +22,23 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
 const dotenv = __importStar(require("dotenv"));
+const node_path_1 = __importDefault(require("node:path"));
+const node_fs_1 = __importDefault(require("node:fs"));
 dotenv.config();
 const client = new discord_js_1.Client({
     intents: [
@@ -36,4 +50,39 @@ const client = new discord_js_1.Client({
 client.once(discord_js_1.Events.ClientReady, (client) => {
     console.log(`${client.user.tag} is logged in now`);
 });
+client.commands = new discord_js_1.Collection();
+const commandsPath = node_path_1.default.join(__dirname, "cmds");
+const commandFiles = node_fs_1.default.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+    const filePath = node_path_1.default.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+        client.commands.set(command.data.name, command);
+    }
+    else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+client.on(discord_js_1.Events.InteractionCreate, (interaction) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!interaction.isChatInputCommand())
+        return;
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+    try {
+        yield command.execute(interaction);
+    }
+    catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            yield interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+        else {
+            yield interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
+}));
 client.login(process.env.BOT_TOKEN);
--- src/cmds/ping.ts
+++ src/cmds/ping.ts
@@ -3,3 +3,10 @@ import { SlashCommandBuilder } from "discord.js";
 const cmd = new SlashCommandBuilder()
   .setName("ping")
   .setDescription("respond with pong");
+
+module.exports = {
+  data: cmd,
+  async execute(interaction: any) {
+    await interaction.reply("pong");
+  },
+};
