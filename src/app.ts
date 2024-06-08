f70daf0c57b8c2b973fffe363a33277bd3a07b00
kaisic1224 <hiddenshadows1224@gmail.com>
2024-06-07 23:50:17 -0400

    test and workflow

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,16 @@ import path from "node:path";
 import fs from "node:fs";
 
 const client = new Client({
-        intents: [
-                GatewayIntentBits.MessageContent,
-                GatewayIntentBits.GuildMessages,
-                GatewayIntentBits.GuildVoiceStates,
-                GatewayIntentBits.Guilds,
-        ],
+  intents: [
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.Guilds,
+  ],
 });
 
 client.once(Events.ClientReady, (client: Client) => {
-        console.log(`${client.user!.tag} is logged in now`);
+  console.log(`${client.user!.tag} is logged in now`);
 });
 
 client.commands = new Collection();
@@ -20,45 +20,54 @@ client.commands = new Collection();
 const commandsFolder = path.join(__dirname, "cmds");
 const commands = fs.readdirSync(commandsFolder);
 
-for (const folder of commands) {
-        const commandsPath = path.join(commandsFolder, folder);
-        const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-        // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-        for (const file of commandFiles) {
-                const filePath = path.join(commandsPath, file);
-                const command = require(filePath);
-                // Set a new item in the Collection with the key as the command name and the value as the exported module
-                if ("data" in command && "execute" in command) {
-                        client.commands.set(command.data.name, command);
-                } else {
-                        console.log(
-                                `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-                        );
-                }
-        }
-
-
-}
+export const getCommands = (commands: string[], commandsFolder: string, client: Client) => {
+  for (const folder of commands) {
+    const commandsPath = path.join(commandsFolder, folder);
+    const commandFiles = fs
+      .readdirSync(commandsPath)
+      .filter((file) => file.endsWith(".js"));
+    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+    for (const file of commandFiles) {
+      const filePath = path.join(commandsPath, file);
+      const command = require(filePath);
+      // Set a new item in the Collection with the key as the command name and the value as the exported module
+      if ("data" in command && "execute" in command) {
+        client.commands.set(command.data.name, command);
+      } else {
+        console.log(
+          `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+        );
+      }
+    }
+  }
+};
+getCommands(commands, commandsFolder, client)
 
 client.on(Events.InteractionCreate, async (interaction) => {
-        if (!interaction.isChatInputCommand()) return;
-        const command = interaction.client.commands.get(interaction.commandName);
+  if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
 
-        if (!command) {
-                console.error(`No command matching ${interaction.commandName} was found.`);
-                return;
-        }
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
 
-        try {
-                await command.execute(interaction);
-        } catch (error) {
-                console.error(error);
-                if (interaction.replied || interaction.deferred) {
-                        await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-                } else {
-                        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-                }
-        }
-})
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
---
+++ tests/play.test.ts
@@ -0,0 +1,67 @@
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import * as path from "path";
+import * as fs from "fs";
+import { getCommands } from '../src/app'
+
+let client;
+const respondInteraction = async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(
+      `No command matching ${interaction.commandName} was found.`
+    );
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+};
+
+describe("testing bot slash commands", () => {
+  beforeAll(() => {
+    client = new Client({
+      intents: [
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.Guilds,
+      ],
+    });
+    expect(process.env.BOT_TOKEN).toBeTruthy();
+
+    // setup from src/app.ts
+
+    client.commands = new Collection();
+
+    const commandsFolder = path.join(__dirname, "../dist/cmds");
+    const commands = fs.readdirSync(commandsFolder);
+
+    getCommands(commands, commandsFolder, client);
+    client.on(Events.InteractionCreate, respondInteraction);
+
+    client.login(process.env.BOT_TOKEN);
+  });
+  afterAll(async () => {
+    client.off(Events.InteractionCreate, respondInteraction);
+    client.destroy();
+  });
+  test("Tests bot play functionality", () => {
+    expect(process.env.CLIENT_ID).toBeTruthy();
+  });
+});
